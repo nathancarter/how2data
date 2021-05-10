94c3c388802e6e21b781587b7ca93c34902addc9
Nathan Carter <ncarter@bentley.edu>
2021-05-10 22:36:32 +0000

    Add database config file

--- build_tools.py
+++ build_tools.py
@@ -1,5 +1,6 @@
 
 import os
+from ruamel.yaml import YAML
 
 def section_heading ( title ):
     print( '-' * len( title ) )
@@ -15,3 +16,6 @@ main_folder = os.path.dirname( os.path.realpath( __file__ ) )
 static_folder = os.path.join( main_folder, 'database', 'static' )
 static_pages = markdown_files( os.listdir( static_folder ) )
 jekyll_input_folder = os.path.join( main_folder, 'jekyll-input' )
+
+with open( os.path.join( main_folder, 'database', 'database.yml' ), 'r' ) as f:
+    configuration = YAML().load( ''.join( f.readlines() ) )
---
+++ database/README.md
@@ -0,0 +1,7 @@
+
+# Specification of `database.yml`
+
+ * The `software` key will map to a list of strings, the software packages
+   coverved by this website (e.g., Python, R, Julia, Excel, SPSS, etc.).
+
+(More to come.)
