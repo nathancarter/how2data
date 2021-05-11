7d60f209847ca1360900e15b05fee618dcc1da01
Nathan Carter <ncarter@bentley.edu>
2021-05-11 15:48:06 +0000

    Create software package page

--- build.py
+++ build.py
@@ -1,14 +1,16 @@
 #!/opt/conda/bin/python
 
-import os, sys, shutil
+import os, sys
 from build_tools import *
 
 section_heading( 'Database build process' )
 for file in static_pages:
-    source = os.path.join( static_folder, file )
-    dest = os.path.join( jekyll_input_folder, file )
-    shutil.copyfile( source, dest )
-    print( f'Copied: {source} -> {dest}' )
+    if file == 'software.md':
+        copy_static_file( file, {
+            'SET_OF_SOFTWARE_PACKAGES': software_table
+        } )
+    else:
+        copy_static_file( file )
 
 section_heading( 'Jekyll build process' )
 sys.exit( os.system( 'bundle exec jekyll build' ) )
---
+++ database/static/software.md
@@ -0,0 +1,12 @@
+---
+layout: page
+title: Software Packages
+permalink: /software/
+---
+
+Here is a list of the software packages that appear in the solutions on this website.
+This list can include standard applications like Microsoft Excel,
+web-based applications like Google Sheets,
+or programming languages like Python and R.
+
+SET_OF_SOFTWARE_PACKAGES
