27b42d802fe691c75db059eca670db4c4e0ed3a5
Nathan Carter <ncarter@bentley.edu>
2021-05-21 10:11:52 -0400

    Read solutions folder, store contents, stop if errors

--- build.py
+++ build.py
@@ -4,6 +4,17 @@ import os, sys
 from build_tools import *
 
 section_heading( 'Database build process' )
+for task, subfolders in solution_docs.items():
+    if not is_a_task( task ):
+        print( 'Build error: Solution folder not named after any existing task' )
+        print( '     Folder:', os.path.join( solutions_folder, task ) )
+        sys.exit( 1 )
+    for software, solutions in subfolders.items():
+        if not is_a_software_package( software ):
+            print( 'Build error: Software folder not named after any existing package' )
+            print( '     Folder:', os.path.join( solutions_folder, task, software ) )
+            print( '   Packages:', ', '.join( software_names ) )
+            sys.exit( 1 )
 for file in static_pages:
     if file == 'software.md':
         copy_static_file( file, {
--- build_tools.py
+++ build_tools.py
@@ -4,29 +4,59 @@ from ruamel.yaml import YAML
 import pandas as pd
 import numpy as np
 
+# What extensions count as documents vs. images?
+# How can we easily filter for those extensions in a list of files?
+doc_extensions = [ '.md', '.markdown' ]
+img_extensions = [ '.jpg', '.jpeg', '.png', '.gif' ]
+def has_any_extension ( filename, extensions ):
+    return any( ( filename.endswith( ext ) for ext in extensions ) )
+def is_doc ( filename ):
+    return has_any_extension( filename, doc_extensions )
+def is_img ( filename ):
+    return has_any_extension( filename, img_extensions )
+def just_docs ( filenames ):
+    return [ x for x in filenames if is_doc( x ) ]
+def just_imgs ( filenames ):
+    return [ x for x in filenames if is_img( x ) ]
+def subfolders ( folder ):
+    return [ x for x in os.listdir( folder ) \
+             if os.path.isdir( os.path.join( folder, x ) ) ]
+def docs_inside ( folder ):
+    return [ x for x in os.listdir( folder ) if is_doc( x ) ]
+def imgs_inside ( folder ):
+    return [ x for x in os.listdir( folder ) if is_img( x ) ]
+
 # Function for printing section headings in the console output
 def section_heading ( title ):
     print( '-' * len( title ) )
     print( title )
     print( '-' * len( title ) )
 
-# Function for filtering a list of filenames for just Markdown files
-def markdown_files ( filenames ):
-    return [
-        x for x in filenames if x[-3:] == '.md' or x[-9:] == '.markdown'
-    ]
-
 # Global variables for important folders in this repo and their contents
 main_folder = os.path.dirname( os.path.realpath( __file__ ) )
 static_folder = os.path.join( main_folder, 'database', 'static' )
-static_pages = markdown_files( os.listdir( static_folder ) )
+static_pages = just_docs( os.listdir( static_folder ) )
 tasks_folder = os.path.join( main_folder, 'database', 'tasks' )
 task_files = [
-    file for file in markdown_files( os.listdir( tasks_folder ) ) \
+    file for file in just_docs( os.listdir( tasks_folder ) ) \
     if file != 'README.md'
 ]
 task_image_files = os.listdir( os.path.join( tasks_folder, 'images' ) )
 jekyll_input_folder = os.path.join( main_folder, 'jekyll-input' )
+solutions_folder = os.path.join( main_folder, 'database', 'solutions' )
+solution_docs = {
+    task : {
+        software : docs_inside( os.path.join( solutions_folder, task, software ) ) \
+        for software in subfolders( os.path.join( solutions_folder, task ) )
+    } \
+    for task in subfolders( solutions_folder )
+}
+solution_imgs = [
+    os.path.join( solutions_folder, task, software, x ) \
+    for task in subfolders( solutions_folder ) \
+    for software in subfolders( os.path.join( solutions_folder, task ) ) \
+    for x in imgs_inside( os.path.join( solutions_folder, task, software ) )
+]
 
 # Read database/database.yml and store it in a global variable
 with open( os.path.join( main_folder, 'database', 'database.yml' ), 'r' ) as f:
@@ -64,18 +94,26 @@ def software_package_website ( package ):
 
 # The software table to be inserted on the software packages page
 software_table = configuration['software']
+software_names = [ software_package_name( x ) for x in software_table ]
 software_table = pd.DataFrame( {
-    "Software Package" : map( software_package_name, software_table ),
+    "Software Package" : software_names,
     "Icon" : map( software_package_icon, software_table ),
     "Number of solutions" : np.nan,
     "Website" : map( software_package_website, software_table )
 } )
 software_table = software_table.to_markdown( index=False )
 
-# The tasks table to be insert on the tasks page
+# The tasks table to be inserted on the tasks page
 tasks_table = pd.DataFrame( {
     "Task" : [ os.path.splitext( filename )[0] for filename in task_files ]
 } )
 for package in configuration['software']:
     tasks_table[f'Solutions in {package["name"]}'] = np.nan
 tasks_table = tasks_table.to_markdown( index=False )
+
+# How to tell if a given string is one of the official names of a task or a
+# software package in our database?  We make some functions.
+def is_a_task ( text ):
+    return any( [ text + ext in task_files for ext in doc_extensions ] )
+def is_a_software_package ( text ):
+    return text in software_names
