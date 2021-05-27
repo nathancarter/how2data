cfef1089537407066673ea0d16e1847390b6a461
Nathan Carter <ncarter@bentley.edu>
2021-05-27 12:33:30 -0400

    Improve output type prioritization

--- jupyter_nbconvert_config.py
+++ jupyter_nbconvert_config.py
@@ -1,11 +1,11 @@
 c.ExtractOutputPreprocessor.enabled = False
 c.NbConvertBase.display_data_priority = [
-     'text/plain',
-     'text/markdown',
-     'text/html',
-     'application/pdf',
-     'text/latex',
-     'image/svg+xml',
-     'image/png',
-     'image/jpeg'
+    # 'text/html',
+    'application/pdf',
+    # 'text/latex',
+    'image/svg+xml',
+    'image/png',
+    'image/jpeg',
+    # 'text/markdown',
+    'text/plain'
 ]
