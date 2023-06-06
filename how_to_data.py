2b90ea109bc6a6153ee50a34f692956e83cccbcf
Nathan Carter <ncarter@bentley.edu>
2023-06-06 15:46:35 -0400

    Bug fix: Examples folder wasn't fully deleted

--- how_to_data.py
+++ how_to_data.py
@@ -70,7 +70,7 @@ def database_to_jekyll ( force_rerun=False, quiet=False, callback=None ):
 
     # Delete files generated in last build
     if quiet:
-        pbar.set_phase( 0, 7 )
+        pbar.set_phase( 0, 6 )
         pbar.set_step( 0, 1 )
     log.heading( 'Deleting old files from Jekyll input folder' )
     to_delete = ' '.join( [
@@ -82,7 +82,7 @@ def database_to_jekyll ( force_rerun=False, quiet=False, callback=None ):
 
     # Copy files to Jekyll input folder
     if quiet:
-        pbar.set_phase( 1, 7 )
+        pbar.set_phase( 1, 6 )
         pbar.set_step( 0, 2 )
     log.heading( 'Copying files to Jekyll input folder' )
     replacements = {
@@ -104,7 +104,7 @@ def database_to_jekyll ( force_rerun=False, quiet=False, callback=None ):
     log.heading( 'Generating files from database content' )
     # Note: solution building must go first so task pages can read the generated results
     if quiet:
-        pbar.set_phase( 2, 7 )
+        pbar.set_phase( 2, 6 )
         pbar.set_step( 0, len( solutions.all() ) )
     for index, row in solutions.all().iterrows():
         solutions.Solution( row ).build( force=force_rerun )
@@ -112,7 +112,7 @@ def database_to_jekyll ( force_rerun=False, quiet=False, callback=None ):
             pbar.step()
     # Now task pages get built second, so they can read the generated solution pages
     if quiet:
-        pbar.set_phase( 3, 7 )
+        pbar.set_phase( 3, 6 )
         pbar.set_step( 0, len( tasks.all() ) )
     for index, row in tasks.all().iterrows():
         tasks.Task( row ).build()
@@ -120,7 +120,7 @@ def database_to_jekyll ( force_rerun=False, quiet=False, callback=None ):
             pbar.step()
     # Create a page for each software package; the sequencing of this step is not important.
     if quiet:
-        pbar.set_phase( 4, 7 )
+        pbar.set_phase( 4, 6 )
         pbar.set_step( 0, len( software.all() ) )
     for index, row in software.all().iterrows():
         software.Software( row ).build()
@@ -128,29 +128,13 @@ def database_to_jekyll ( force_rerun=False, quiet=False, callback=None ):
             pbar.step()
     # Create a page for each topic; the sequencing of this step is not important.
     if quiet:
-        pbar.set_phase( 5, 7 )
+        pbar.set_phase( 5, 6 )
         pbar.set_step( 0, len( topics.all() ) )
     for index, row in topics.all().iterrows():
         topics.Topic( row ).build()
         if quiet:
             pbar.step()
     files.delete_ungenerated_markdown()
-
-    # Zip up examples to reference from the Contributing page
-    if quiet:
-        pbar.set_phase( 6, 7 )
-        pbar.set_step( 0, 1 )
-    log.heading( 'Zipping examples for contributors' )
-    infiles = glob.iglob( 'examples/*', recursive=True )
-    outfile = 'jekyll-input/assets/downloads/examples-for-contributing-to-how-to-data.zip'
-    if any( files.must_rebuild( infile, outfile ) for infile in infiles ):
-        howto = 'examples/How to use this folder'
-        shell.run_or_halt( f'pandoc --to=docx --output="{howto}.docx" "{howto}.md"' )
-        files.ensure_folder_exists(
-            os.path.join( jekyll_input_folder, 'assets', 'downloads' ) )
-        shell.run_or_halt( f'cd examples && zip -orv9 "../{outfile}" *' )
-    else:
-        log.not_built( 'Examples for contributors', *infiles )
 
     if quiet:
         pbar.end()
