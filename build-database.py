5f501113b92da506b0c881cdf239f81745c540af
Nathan Carter <ncarter@bentley.edu>
2024-01-17 10:22:34 -0500

    Fix bugs in database archive process, rebuild archive

--- build-database.py
+++ build-database.py
@@ -37,25 +37,27 @@ pbar = progress.ProgressBar( 'Tasks table' )
 pbar.set_step( 0, len( tasks_df ) )
 for index, row in tasks_df.iterrows():
     task = tasks.Task( row )
-    row['url'] = task.url
-    row['description'] = task.description()
-    row['generated'] = task.generated_markdown()
+    tasks_df.at[index,'url'] = task.url
+    tasks_df.at[index,'description'] = task.description()
+    tasks_df.at[index,'generated'] = task.generated_markdown()
     pbar.step()
 pbar.end()
 log.info( 'Extended tasks table' )
 
 topics_df['url'] = ''
 topics_df['tasks'] = np.nan
+topics_df['tasks'] = topics_df['tasks'].astype( object )
 topics_df['generated'] = ''
 topics_df['pdf urls'] = np.nan
+topics_df['pdf urls'] = topics_df['pdf urls'].astype( object )
 pbar = progress.ProgressBar( 'Topics table' )
 pbar.set_step( 0, len( topics_df ) )
 for index, row in topics_df.iterrows():
     topic = topics.Topic( row )
-    row['url'] = topic.url
-    row['tasks'] = topic.tasks()['task name'].tolist()
-    row['generated'] = topic.generated_markdown()
-    row['pdf urls'] = [ topics.Topic.pdf_url( file ) for file in topic.existing_pdf_files() ]
+    topics_df.at[index,'url'] = topic.url
+    topics_df.at[index,'tasks'] = topic.tasks()['task name'].tolist()
+    topics_df.at[index,'generated'] = topic.generated_markdown()
+    topics_df.at[index,'pdf urls'] = [ topics.Topic.pdf_url( file ) for file in topic.existing_pdf_files() ]
     pbar.step()
 pbar.end()
 log.info( 'Extended topics table' )
@@ -68,10 +70,10 @@ pbar = progress.ProgressBar( 'Solutions table' )
 pbar.set_step( 0, len( solutions_df ) )
 for index, row in solutions_df.iterrows():
     solution = solutions.Solution( row )
-    row['url'] = solution.url
-    row['inner content'] = solution.inner_content
-    row['generated'] = solution.generated_markdown()
-    row['generated interior only'] = solution.generated_body()
+    solutions_df.at[index,'url'] = solution.url
+    solutions_df.at[index,'inner content'] = solution.inner_content
+    solutions_df.at[index,'generated'] = solution.generated_markdown()
+    solutions_df.at[index,'generated interior only'] = solution.generated_body()
     pbar.step()
 pbar.end()
 solutions_df = solutions_df.astype( str ) # prevent warnings when saving to HDF; whole table is strings anyway
